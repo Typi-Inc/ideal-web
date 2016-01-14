6fb952d6b30840a32f5cdf03d591574cb9d9800c
YesaDyuss <iskandermm@gmail.com>
2016-01-14 19:26:53 +0600

    added conditions card to create deal

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import Radium from 'radium';
 import Link from './Link';
+import CreateDealAddTags from './CreateDealAddTags';
 
 const styles = {
   card: {
     margin: '10px',
     background: '#fff',
     padding: '10px'
+  },
+  tagBorder: {
+    fontSize: '14px',
+    borderRadius: '5px',
+    minWidth: '40px',
+    margin: '10px 5px 10px 0',
+    padding: '5px',
+    textAlign: 'center',
+    color: '#777777',
+    background: '#fff',
+    border: '1.5px solid #eee'
   }
 };
 
 class Settings extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      tagNames : []
+    };
+  }
+
+  postTag() {
+    const tagName = this.tagName.value;
+    if(tagName !=='') {
+      this.setState({
+        tagNames: this.state.tagNames.concat([tagName])
+      });
+      this.tagName.value = '';
+    }
+  }
+
+  removeTag(tag) {
+    this.setState({
+      tagNames: this.state.tagNames.filter(t =>!(t===tag))
+    })
+  }
 
   render() {
 
@@ -23,99 +58,88 @@ class Settings extends React.Component {
                   display: 'flex',
                   justifyContent: 'space-between',
                   alignItems: 'center',
-                  borderBottom: '1px solid rgba(0,0,0,.12)',
                   paddingBottom: '10px'
                        }}>
             <button className="mdl-button mdl-js-button mdl-js-ripple-effect" style = {{height: '50px'}}>
               <i className="material-icons" style = {{ color: '#a99999 ', fontSize: '50px' }}>account_box</i>
             </button>
-            <span style = {{ fontWeight: '500', fontSize: '20px', textAlign: 'center', paddingRight: '30px' }}>Your Profile</span>
+            <span style = {{
+              fontWeight: '500',
+              fontSize: '20px',
+              textAlign: 'center',
+              color: '#777777',
+              paddingRight: '30px'
+                            }}>Your Profile</span>
           </div>
-          <form action="#">
-            <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" >
-              <textarea className="mdl-textfield__input" type="text" rows= "2" id="email" ></textarea>
-              <label className="mdl-textfield__label" htmlFor="email">Email</label>
-            </div>
-          </form>
+
         </div>
         <div style = {styles.card}>
           <div  style = {{
                   fontWeight: '500',
                   fontSize: '20px',
+                  color: '#777777',
                   textAlign: 'center',
-                  paddingBottom: '10px',
-                  borderBottom: '1px solid rgba(0,0,0,.12)' }}>
+                  paddingBottom: '10px'
+                   }}>
             Your tags
           </div>
-          <div style = {{ display : 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <div style = {{ width: '70%'}}>
-              <form action="#">
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                  <input className="mdl-textfield__input" type="text" id="tags"/>
-                  <label className="mdl-textfield__label" htmlFor="tags">Add preferable deal types</label>
-                </div>
-              </form>
+          <div style = {{ display: 'flex', flexWrap: 'wrap', paddingLeft: '10px' }}>
+            {this.state.tagNames.map(tagName => (
+              <div style={ styles.tagBorder }>
+                {tagName}
+                <button className="mdl-button mdl-js-button mdl-js-ripple-effect"
+                        onClick = {this.removeTag.bind(this, tagName)}
+                        style = {{ lineHeight: '20px', height: '20px', minWidth: '20px', padding: '0' }}
+                  >
+                  <i className="material-icons" style = {{ fontSize: '14px' }}>remove</i>
+                </button>
+              </div>
+            ))}
+          </div>
+          <div style ={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div style = {{ width: '90%' }}>
+              <input type="text"
+                     ref={el => this.tagName = el}
+                     placeholder="Autoservice, Bakery, Bar, e.g."
+                     style = {{
+                           border: 'solid 1px #dcdcdc',
+                           borderRadius: '3px',
+                           height: '32px',
+                           paddingLeft: '10px',
+                           width: '90%'
+                           }}/>
             </div>
-            <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
-                    style= {{
-                        width: '28px',
-                        height: '28px',
+            <div>
+              <button className="mdl-button mdl-js-button mdl-js-ripple-effect"
+                      onClick={this.postTag.bind(this)}
+                      style= {{
                         background: '#0679A2',
                         minWidth: '28px',
-                        padding: '0px',
-                        margin: '0 5px'
-                      }}>
-              <i className="material-icons" style = {{color:'white', paddingBottom: '10px'}} >add</i>
-            </button>
+                        padding: '0 5px',
+                        borderRadius: '3px',
+                        textTransform: 'none'
+                                 }}
+                >
+                <span style = {{ color: 'white' }} >Add</span>
+              </button>
+            </div>
           </div>
         </div>
-        <div style = {{
-              display: 'flex',
-              justifyContent: 'flex-end',
-              background: '#dedede',
-              paddingBottom: '10px',
-              margin : '0 10px'
-                     }}>
-          <div style = {{width: '10%'}}>
-            <button className="mdl-button mdl-js-button mdl-js-ripple-effect"
-                    style={{background: '#0679A2', width: '100%', color: 'white', borderRadius: '10px', boxShadow: '0'}}>
-              Save
-            </button>
-          </div>
-        </div>
+          <button className="mdl-button mdl-js-button mdl-js-ripple-effect"
+                  style={{
+                    background: '#fff',
+                    color: '#0679a2',
+                    marginRight: '10px',
+                    float: 'right',
+                    fontSize: '18px',
+                    textTransform: 'none',
+                    borderRadius: '3px',
+                    boxShadow: '0'}}>
+            Save
+          </button>
       </div>
     )
   }
 }
 
 export default Radium(Settings);
-
-//<div style = {styles.card}>
-//  <div  style = {{
-//                  fontWeight: '500',
-//                  fontSize: '20px',
-//                  textAlign: 'center',
-//                  paddingBottom: '10px',
-//                  borderBottom: '1px solid rgba(0,0,0,.12)' }}>
-//    Private Info
-//  </div>
-//  <div style = {{ display : 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-//    <div style = {{ width: '40%'}}>
-//      <form action="#">
-//        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-//          <input className="mdl-textfield__input" type="text" id="gender0"/>
-//          <label className="mdl-textfield__label" htmlFor="gender">Gender</label>
-//        </div>
-//      </form>
-//    </div>
-//    <div style = {{ width: '40%'}}>
-//      <form action="#">
-//        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-//          <input className="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" id="birthYear"/>
-//          <label className="mdl-textfield__label" htmlFor="birthYear">Year of birth</label>
-//          <span className="mdl-textfield__error">It's not a number!</span>
-//        </div>
-//      </form>
-//    </div>
-//  </div>
-//</div>
