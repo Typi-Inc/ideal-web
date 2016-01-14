ac24f8f55971a8d486f6420f4c76d37157febf95
Almas Akchabayev <almas.akchabayev@gmail.com>
2016-01-14 20:22:23 +0600

    added StyleRoot for radium to work

---
+++ src/components/AppBodyWrapper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Radium, { StyleRoot } from 'radium';
+import AppBody from './AppBody';
+
+class AppBodyWrapper extends React.Component {
+  render() {
+    return (
+      <StyleRoot>
+        <AppBody {...this.props} />
+      </StyleRoot>
+    );
+  }
+}
+
+export default Radium(AppBodyWrapper);
