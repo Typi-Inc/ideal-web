56bc9802080270b6be1c8a5e709905bfe5c70bad
Almas Akchabayev <almas.akchabayev@gmail.com>
2016-02-01 22:17:46 +0600

    react-lis

--- src/components/Deals.js
+++ src/components/Deals.js
@@ -1,65 +1,32 @@
 import React from 'react';
 import Radium from 'radium';
-import Link from './Link';
 import _ from 'lodash';
-import { values } from '../utils/helpers';
-import FontIcon from 'material-ui/lib/font-icon';
-import FlatButton from 'material-ui/lib/flat-button';
-import '../public/hover.css';
+import DealsItem from './DealsItem';
+import { get } from '../intent/get';
 import ReactList from 'react-list';
 
 const RadiumReactList = Radium(ReactList);
-
-const styles = {
-  card: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    margin: '8px 10px 5px 10px',
-    width: '100%',
-    background: '#fff'
-
-  },
-  dealImage: {
-    backgroundSize: '100% 100%',
-    height: '200px',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-    '@media (min-width: 850px)': {
-      height: '250px'
-    },
-    '@media (min-width: 1020px)': {
-      height: '180px'
-    }
-  },
-  mainOptionsCard: {
-    display: 'flex',
-    width: '50px',
-    justifyContent: 'center',
-    background: '#0679A2',
-    height: '50px',
-    color: '#fff',
-    alignItems: 'center',
-    fontWeight: '500'
-  },
-  tagBorder: {
-    fontSize: '12px',
-    borderRadius: '5px',
-    lineHeight: '18px',
-    minWidth: '40px',
-    padding: '6px 13px',
-    margin: '0 5px 5px 0',
-    textTransform: 'none',
-    height: '32px',
-    background: '#fff',
-    border: '2px solid #eee'
-  }
-};
 
 class Deals extends React.Component {
   static propTypes = {
     deals: React.PropTypes.array
   };
+  state = { isLoading: false };
+
+  componentWillMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ isLoading: false });
+    this.pending = [];
+    return nextProps;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll.bind(this));
+  }
+
   static queries = {
     deal() {
       return [['title', 'conditions', 'id', 'image', 'discount']];
@@ -68,7 +35,7 @@ class Deals extends React.Component {
       return ['business', ['name', 'image']];
     },
     tags() {
-      return ['tags', 'sort:createdAt=desc', 'edges', {from: 0, to: 6}, ['id', 'text']];
+      return ['tags', 'sort:createdAt=desc', 'edges', { from: 0, to: 6 }, ['id', 'text']];
     },
     likes() {
       return ['likes', 'sort:createdAt=desc', 'count'];
@@ -77,134 +44,60 @@ class Deals extends React.Component {
       return ['likedByUser', '{{me}}'];
     }
   };
-  renderItem(index, key) {
-    const deal = this.props.deals[index];
-    console.log(JSON.stringify(deal, null, 4), 'deal is here');
+  pending = [];
+
+  request(from) {
+    this.pending.push(from);
+    this.fetch();
+  }
+
+  fetch() {
+    if (this.isLoading || !this.pending.length) return;
+    const from = _.first(this.pending);
+    get([
+      ['featuredDeals', { from: from, to: from + 9 }, ['title', 'id', 'discount', 'image']],
+      ['featuredDeals', { from: from, to: from + 9 }, 'business', ['name', 'image']],
+      ['featuredDeals', { from: from, to: from + 9 }, 'tags', 'sort:createdAt=desc', 'edges',
+        { from: 0, to: 6 }, ['id', 'text']],
+      ['featuredDeals', { from: from, to: from + 9 }, 'likes', 'sort:createdAt=desc', 'count'],
+      ['featuredDeals', { from: from, to: from + 9 }, 'likedByMe']
+    ]);
+    this.setState({ isLoading: true });
+  }
+  handleScroll() {
+    const [, lastVisibleIndex] = this.list.getVisibleRange();
+    if (lastVisibleIndex >= this.props.deals.length - 2) this.request(this.props.deals.length);
+  }
+  renderItems(items, ref) {
     return (
-      <div key={deal.id}
-        style={{ width: '100%', '@media (min-width: 1020px)': { width: '480px', marginLeft: '10px' } }}
+      <div
+        ref={ref}
+        style = {{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', width: '100%' }}
       >
-        <div style={{ display: 'none',
-          '@media (min-width: 580px)': {
-            display: 'block',
-            background: '#fff',
-            margin: '10px 10px 0 10px',
-            width: '100%' }
-        }}
-        >
-          <div style={{
-            padding: '10px',
-            fontStyle: 'italic',
-            fontSize: '13px'
-          }}
-          >
-            <span style={{ color: '#a99999' }}>Published by </span>
-            <span>{_.get(deal, ['business', 'name'])}</span>
-          </div>
-        </div>
-        <div id="hoverCard" style={styles.card}>
-          <div style={{ width: '100%', '@media (min-width: 580px)': { display: 'none' } }}>
-            <div style={{
-              padding: '10px',
-              fontStyle: 'italic',
-              fontSize: '13px'
-            }}>
-              <span style={{ color: '#a99999' }}>Published by </span>
-              <span>{_.get(deal, ['business', 'name'])}</span>
-            </div>
-          </div>
-
-          <div style={{ width: '100%', '@media (min-width: 580px)': { width: '50%' } }}>
-            <Link to={'/deal/' + deal.id}>
-              <div style={[styles.dealImage, {
-                backgroundImage: `url("${deal.image}")`
-              }]}
-              >
-                {
-                  deal.discount && (
-                    <div style={ styles.mainOptionsCard }>
-                      <div style={{ paddingLeft: '5px' }}>
-                        <span style={{ fontSize: '18px' }}>
-                          -{deal.discount}
-                        </span>
-                        <span style={{ fontSize: '14px' }}>%</span>
-                      </div>
-                    </div>
-                  )
-                }
-              </div>
-            </Link>
-          </div>
-
-          <div style={{ width: '100%', '@media (min-width: 580px)': { width: '50%' } }}>
-            <div style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              width: '100%'
-            }}
-            >
-              <div style={{ padding: '10px' }}>
-                <span>
-                  {deal.title}
-                </span>
-              </div>
-            </div>
-
-            <div style={{ padding: '0 10px 10px 10px' }}>
-              <FontIcon className="material-icons" color="green"
-                style={{ fontSize: '14px', padding: '0 5px' }}
-              >shopping_cart</FontIcon>
-              {'?'}
-              <FontIcon className="material-icons" color="red"
-                style={{ fontSize: '14px', padding: '0 5px' }}
-              >favorite</FontIcon>
-              {_.get(deal, ['likes', 'sort:createdAt=desc', 'count'], 0)}
-              <img src="/src/public/assets/hand132-5.png"
-                style={{ height: '14px', padding: '0 5px' }}/>
-              {'?'}
-            </div>
-            {
-              values(_.get(deal, ['tags', 'sort:createdAt=desc', 'edges'])).
-                map(tag => (
-                  <div key={`${deal.id}${tag.id}`}
-                    style={{
-                      width: '95%',
-                      padding: '0 10px 10px 10px',
-                      fontSize: '10px',
-                      color: '#a99999'
-                    }}
-                  >
-                    <FlatButton color="#777777" label={tag.text}
-                      style={styles.tagBorder}
-                    />
-                  </div>
-                ))
-            }
-          </div>
-        </div>
+        {items}
       </div>
+    );
+  }
+  renderItem(index, key) {
+    return (
+      <DealsItem key={key} deal={this.props.deals[index]} />
     );
   }
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          maxWidth: '1500px',
-          margin: 'auto'
-        }}
-      >
-        <div>
-          <RadiumReactList
-            ref={el => this.list = el}
-            itemRenderer={this.renderItem.bind(this)}
-            length={this.props.deals.length}
-            type="variable"
-          />
-        </div>
+      <div>
+        <RadiumReactList
+          ref={el => this.list = el}
+          itemRenderer={this.renderItem.bind(this)}
+          itemsRenderer={this.renderItems.bind(this)}
+          length={this.props.deals.length}
+          type="variable"
+        />
+        {
+          this.state.isLoading && (
+            <div>Loading</div>
+          )
+        }
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -31,12 +31,12 @@ class Home extends React.Component {
         {
           this.context.model$.
             flatMap(model => Observable.fromPromise(model.get(
-              ['featuredDeals', { from: 0, to: 9 }, ['title', 'id', 'discount', 'image']],
-              ['featuredDeals', { from: 0, to: 9 }, 'business', ['name', 'image']],
-        			['featuredDeals', { from: 0, to: 9 }, 'tags', 'sort:createdAt=desc', 'edges',
+              ['featuredDeals', { from: 0, to: 119 }, ['title', 'id', 'discount', 'image']],
+              ['featuredDeals', { from: 0, to: 119 }, 'business', ['name', 'image']],
+        			['featuredDeals', { from: 0, to: 119 }, 'tags', 'sort:createdAt=desc', 'edges',
                 { from: 0, to: 6 }, ['id', 'text']],
-        			['featuredDeals', { from: 0, to: 9 }, 'likes', 'sort:createdAt=desc', 'count'],
-        			['featuredDeals', { from: 0, to: 9 }, 'likedByMe']
+        			['featuredDeals', { from: 0, to: 119 }, 'likes', 'sort:createdAt=desc', 'count'],
+        			['featuredDeals', { from: 0, to: 119 }, 'likedByMe']
             ))).
             map(data => console.log(count++)||<Deals deals={values(data.json.featuredDeals)} />)
         }
